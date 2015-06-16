95e69e46934fd1b083cd781443c42246dd382793
Matt VanderKolk <jmvdkolk@gmail.com>
2015-06-16 04:10:32 -0700

    Added election logic. - Added Election class to handle election logic. - Added unit test project. - Downloaded Moq package. - Added unit tests for voting logic.

---
+++ InstantRunoffVoter/Voting/Election.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantRunoffVoter.Voting
+{
+    /// <summary>
+    /// Class representing a single election process.
+    /// </summary>
+    public class Election
+    {
+        /// <summary>
+        /// The collection of voters who are voting in this election.
+        /// </summary>
+        private readonly ReadOnlyCollection<string> voters;
+
+        /// <summary>
+        /// The collection of candidates being voted on in this election.
+        /// </summary>
+        private readonly ReadOnlyCollection<string> candidates;
+
+        /// <summary>
+        /// The ordered list of voters who have not voted yet.
+        /// </summary>
+        private readonly List<string> votersWithoutVotes;
+
+        /// <summary>
+        /// The ballot results for the voters. The key is the voter name.
+        /// </summary>
+        private readonly Dictionary<string, List<string>> ballots;
+
+        /// <summary>
+        /// The number of voters required to vote for a single candidate for it to win.
+        /// </summary>
+        private readonly int requiredMajority;
+
+        /// <summary>
+        /// Random number generator to use.
+        /// </summary>
+        private Random random;
+
+        /// <summary>
+        /// The winner of the election once it is known.
+        /// </summary>
+        private string winner;
+
+        /// <summary>
+        /// Initializes a new instance of the Election class with a given set of voters and candidates.
+        /// </summary>
+        /// <param name="voters">The set of voters voting in this election.</param>
+        /// <param name="candidates">The set of candidates being voted on in this election.</param>
+        public Election(IEnumerable<string> voters, IEnumerable<string> candidates)
+        {
+            List<string> votersTemp = new List<string>(voters.Distinct());
+            votersTemp.Sort();
+            this.voters = votersTemp.AsReadOnly();
+
+            List<string> candidatesTemp = new List<string>(candidates.Distinct());
+            candidatesTemp.Sort();
+            this.candidates = candidatesTemp.AsReadOnly();
+
+            if (this.voters.Count < 2 || this.candidates.Count < 2)
+            {
+                throw new ArgumentException("The voters and/or candidates list do not make a valid election.");
+            }
+
+            this.votersWithoutVotes = new List<string>(this.voters);
+
+            this.ballots = new Dictionary<string, List<string>>();
+
+            this.requiredMajority = (int)Math.Floor((this.voters.Count / 2.0) + 1);
+
+            this.random = new Random();
+        }
+
+        /// <summary>
+        /// Gets a sorted list of voters who have not voted in this election yet.
+        /// </summary>
+        public IEnumerable<string> VotersWithoutVotes
+        {
+            get
+            {
+                List<string> votersCopy = new List<string>(this.votersWithoutVotes);
+                votersCopy.Sort();
+                return votersCopy;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of voters in this election who have not voted yet.
+        /// </summary>
+        public int CountVotersWithoutVotes
+        {
+            get
+            {
+                return this.votersWithoutVotes.Count;
+            }
+        }
+
+        /// <summary>
+        /// Gets the winner of this election.
+        /// </summary>
+        public string Winner
+        {
+            get
+            {
+                if (this.winner == null)
+                {
+                    throw new InvalidOperationException("The winner of this election is not known because not all ballots have been received.");
+                }
+
+                return this.winner;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the random object to be used.
+        /// </summary>
+        public Random Random
+        {
+            get
+            {
+                return this.random;
+            }
+
+            set
+            {
+                this.random = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the candidates that are being voted on in this election.
+        /// </summary>
+        public ReadOnlyCollection<string> Candidates
+        {
+            get
+            {
+                return this.candidates;
+            }
+        }
+
+        /// <summary>
+        /// Returns the next voter that still needs to vote.
+        /// </summary>
+        /// <returns>The next voter that needs to vote.</returns>
+        public string GetNextVoter()
+        {
+            if (this.votersWithoutVotes.Count == 0)
+            {
+                throw new IndexOutOfRangeException("There are no more voters left who haven't voted.");
+            }
+
+            return this.votersWithoutVotes[0];
+        }
+
+        /// <summary>
+        /// Skips the voter at the front of the list and places them at the back of the list.
+        /// </summary>
+        /// <returns>The voter at the front of the list after skiping the first voter.</returns>
+        public string SkipNextVoter()
+        {
+            if (this.votersWithoutVotes.Count < 2)
+            {
+                throw new IndexOutOfRangeException(string.Format("There aren't enough voters left who haven't voted: {0}.", this.votersWithoutVotes.Count));
+            }
+
+            this.votersWithoutVotes.Add(this.votersWithoutVotes[0]);
+            this.votersWithoutVotes.RemoveAt(0);
+
+            return this.votersWithoutVotes[0];
+        }
+
+        /// <summary>
+        /// Stores the preferences of the current voter.
+        /// </summary>
+        /// <param name="voterPreferences">The ordered preference list for the voter.</param>
+        public void AddBallot(IEnumerable<string> voterPreferences)
+        {
+            if (this.votersWithoutVotes.Count == 0)
+            {
+                throw new IndexOutOfRangeException("There are no voters for which we don't have a ballot for.");
+            }
+
+            if (voterPreferences.Count() != this.candidates.Count)
+            {
+                throw new ArgumentException("This set of preferences does not match.");
+            }
+
+            if (!voterPreferences.OrderBy(preference => preference).SequenceEqual(this.candidates))
+            {
+                throw new ArgumentException("This set of preferences does not match.");
+            }
+
+            // Make sure to perform a copy to avoid weirdness
+            this.ballots[this.GetNextVoter()] = new List<string>(voterPreferences);
+            this.votersWithoutVotes.RemoveAt(0);
+
+            if (this.votersWithoutVotes.Count == 0)
+            {
+                this.winner = this.ComputerWinner();
+            }
+        }
+
+        private string ComputerWinner()
+        {
+            Debug.Assert(this.ballots.Count == this.voters.Count);
+
+            Dictionary<string, List<string>> ballots = this.CopyBallots(this.ballots);
+            Dictionary<string, List<int>> preferences = this.GeneratePreferenceDictionary(ballots);
+
+            string winner;
+            while ((winner = this.FindWinner(preferences)) == null)
+            {
+                string loser = this.FindLoser(preferences);
+                ballots = this.EliminateLoser(ballots, loser);
+                preferences = this.GeneratePreferenceDictionary(ballots);
+            }
+
+            return winner;
+        }
+
+        /// <summary>
+        /// Performs a deep copy on a set of ballots and returns the copy.
+        /// </summary>
+        /// <param name="ballotsToCopy">The ballots to copy.</param>
+        /// <returns>A deep copy.</returns>
+        Dictionary<string, List<string>> CopyBallots(Dictionary<string, List<string>> ballotsToCopy)
+        {
+            var copy = new Dictionary<string, List<string>>();
+
+            foreach (KeyValuePair<string, List<string>> ballot in ballotsToCopy)
+            {
+                copy[ballot.Key] = new List<string>(ballot.Value);
+            }
+
+            return copy;
+        }
+
+        /// <summary>
+        /// Takes all the voter's ballots and returns a dictionary containing each candidate's preference values.
+        /// </summary>
+        /// <param name="ballots">The voter's ballots to convert.</param>
+        /// <returns>The candidate preference values.</returns>
+        private Dictionary<string, List<int>> GeneratePreferenceDictionary(Dictionary<string, List<string>> ballots)
+        {
+            var preferences = new Dictionary<string, List<int>>();
+            foreach (IEnumerable<string> voterBallot in ballots.Values)
+            {
+                int rank = 1;
+                foreach (string candidate in voterBallot)
+                {
+                    if (!preferences.ContainsKey(candidate))
+                    {
+                        preferences[candidate] = new List<int>();
+                    }
+
+                    preferences[candidate].Add(rank++);
+                }
+            }
+
+            return preferences;
+        }
+
+        /// <summary>
+        /// Determines if any of the candidates have won based on the given set of preference values.
+        /// </summary>
+        /// <param name="preferences">The preferences for the candidates left.</param>
+        /// <returns>The name of the winning candidate if it won. null if no candidates won.</returns>
+        private string FindWinner(Dictionary<string, List<int>> preferences)
+        {
+            foreach (KeyValuePair<string, List<int>> candidatePreferences in preferences)
+            {
+                if (candidatePreferences.Value.Count(preference => preference == 1) >= this.requiredMajority)
+                {
+                    return candidatePreferences.Key;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Determines the biggest loser in a given preferences set and returns its name.
+        /// </summary>
+        /// <param name="preferences">The preferences set to analyze.</param>
+        /// <returns>The name of the candidate that should be removed.</returns>
+        private string FindLoser(Dictionary<string, List<int>> preferences)
+        {
+            // First figure out the current lowest value for number of #1 votes any candidate has
+            int lowestTopVotes = this.voters.Count;
+            foreach (List<int> candidatePreferences in preferences.Values)
+            {
+                int topVotes = candidatePreferences.Count(preference => preference == 1);
+                if (topVotes < lowestTopVotes)
+                {
+                    lowestTopVotes = topVotes;
+                }
+            }
+
+            // Now pull out all candidates that have the lowest preference and remember the sum of their preferences as well
+            Dictionary<string, int> lowestPreferences = new Dictionary<string,int>();
+            foreach (KeyValuePair<string, List<int>> candidatePreferences in preferences)
+            {
+                if (candidatePreferences.Value.Count(preference => preference == 1) == lowestTopVotes)
+                {
+                    lowestPreferences[candidatePreferences.Key] = candidatePreferences.Value.Sum();
+                }
+            }
+
+            // Figure out which of the losers is the biggest loser
+            int biggestLoss = lowestPreferences.Values.Max();
+            List<string> biggestLosers = new List<string>(lowestPreferences.Where(candidate => candidate.Value == biggestLoss).Select(candidate => candidate.Key));
+            string loser;
+            if (biggestLosers.Count == 1)
+            {
+                loser = biggestLosers[0];
+            }
+            else
+            {
+                // Looks like something must be eliminated by pure random chance...
+                // Sort this list for testing determinism.
+                biggestLosers.Sort();
+                loser = biggestLosers[this.random.Next(biggestLosers.Count)];
+            }
+
+            return loser;
+        }
+
+        /// <summary>
+        /// Takes a set of ballots, copies them and then returns a new set of ballots with the indicates loser candidate removed.
+        /// </summary>
+        /// <param name="ballots">The ballots to start with.</param>
+        /// <param name="loser">The loser candidate to remove.</param>
+        /// <returns>The new ballots to use.</returns>
+        private Dictionary<string, List<string>> EliminateLoser(Dictionary<string, List<string>> ballots, string loser)
+        {
+            var newBallots = this.CopyBallots(ballots);
+
+            foreach (List<string> newBallot in newBallots.Values)
+            {
+                newBallot.Remove(newBallot.First(candidate => candidate == loser));
+            }
+
+            return newBallots;
+        }
+    }
+}
---
+++ UnitTests/Voting/ElectionTests.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InstantRunoffVoter.Voting;
+using System.Collections.Generic;
+using Moq;
+
+namespace UnitTests.Voting
+{
+    /// <summary>
+    /// Test class for testing Election class.
+    /// </summary>
+    [TestClass]
+    public class ElectionTests
+    {
+        /// <summary>
+        /// The mock Random object that is given to the Election instance.
+        /// </summary>
+        private Mock<Random> mockRandom;
+        
+        /// <summary>
+        /// Initializes the class before every test.
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.mockRandom = new Mock<Random>(MockBehavior.Strict);
+        }
+
+        /// <summary>
+        /// Cleans up the class after every test.
+        /// </summary>
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.mockRandom.VerifyAll();
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 2 candidates where both voters vote the same and candidate 1 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestDeterministic2By2Where1Wins()
+        {
+            Election election = this.GenerateElection(2, 2);
+
+            election.AddBallot(this.FillCandidateNames(1, 2));
+            election.AddBallot(this.FillCandidateNames(1, 2));
+
+            Assert.AreEqual("Candidate 1", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 2 candidates where both voters vote the same and candidate 2 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestDeterministic2By2Where2Wins()
+        {
+            Election election = this.GenerateElection(2, 2);
+
+            election.AddBallot(this.FillCandidateNames(2, 1));
+            election.AddBallot(this.FillCandidateNames(2, 1));
+
+            Assert.AreEqual("Candidate 2", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 2 candidates where both voters vote differently and candidate 1 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestTied2By2Where1Wins()
+        {
+            Election election = this.GenerateElection(2, 2);
+
+            this.mockRandom.Setup(random => random.Next(2)).Returns(1);
+
+            election.AddBallot(this.FillCandidateNames(1, 2));
+            election.AddBallot(this.FillCandidateNames(2, 1));
+
+            Assert.AreEqual("Candidate 1", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 2 candidates where both voters vote differently and candidate 2 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestTied2By2Where2Wins()
+        {
+            Election election = this.GenerateElection(2, 2);
+
+            this.mockRandom.Setup(random => random.Next(2)).Returns(0);
+
+            election.AddBallot(this.FillCandidateNames(1, 2));
+            election.AddBallot(this.FillCandidateNames(2, 1));
+
+            Assert.AreEqual("Candidate 2", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 3 candidates where all voters vote the same and candidate 1 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestDeterministic2By3Where1Wins()
+        {
+            Election election = this.GenerateElection(2, 3);
+
+            election.AddBallot(this.FillCandidateNames(1, 2, 3));
+            election.AddBallot(this.FillCandidateNames(1, 2, 3));
+
+            Assert.AreEqual("Candidate 1", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 3 candidates where 1 loses, 2 and 3 tie and candidate 2 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestTied2By3Where2Wins()
+        {
+            Election election = this.GenerateElection(2, 3);
+
+            this.mockRandom.Setup(random => random.Next(2)).Returns(1);
+
+            election.AddBallot(this.FillCandidateNames(3, 2, 1));
+            election.AddBallot(this.FillCandidateNames(2, 3, 1));
+
+            Assert.AreEqual("Candidate 2", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 2 voters and 3 candidates where 1 loses, 2 and 3 tie and candidate 3 wins.
+        /// </summary>
+        [TestMethod]
+        public void TestTied2By3Where3Wins()
+        {
+            Election election = this.GenerateElection(2, 3);
+
+            this.mockRandom.Setup(random => random.Next(2)).Returns(0);
+
+            election.AddBallot(this.FillCandidateNames(3, 2, 1));
+            election.AddBallot(this.FillCandidateNames(2, 3, 1));
+
+            Assert.AreEqual("Candidate 3", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests a complicated vote where candidate 1 starts off with less #1 votes than candidates 3 and 4 but still
+        /// ends up winning because when candidate 2 is cut, candidate 1 gains a #1 vote.
+        /// 
+        /// Preferences step[0]:
+        ///      C1 | C2 | C3 | C4
+        ///     -------------------
+        ///      1  | 2  | 3  | 4
+        ///      2  | 1  | 3  | 4
+        ///      2  | 4  | 1  | 3
+        ///      3  | 4  | 1  | 2
+        ///      2  | 4  | 3  | 1
+        ///      4  | 2  | 3  | 1
+        ///     -------------------
+        /// #1s: 1  | 1  | 2  | 2
+        /// Sum: 14 | 17 | 14 | 15
+        /// 
+        /// Preferences step[1]:
+        ///      C1 | C3 | C4
+        ///     --------------
+        ///      1  | 2  | 3
+        ///      1  | 2  | 3
+        ///      2  | 1  | 3
+        ///      3  | 1  | 2
+        ///      2  | 3  | 1
+        ///      3  | 2  | 1
+        ///     --------------
+        /// #1s: 2  | 2  | 2
+        /// Sum: 12 | 11 | 13
+        /// 
+        /// Preferences step[2] (Requires random):
+        ///      C1 | C3
+        ///     ---------
+        ///      1  | 2
+        ///      1  | 2
+        ///      1  | 2
+        ///      2  | 1
+        ///      2  | 1
+        ///      2  | 1
+        ///     ---------
+        /// #1s: 3  | 3
+        /// Sum: 9  | 9 
+        /// </summary>
+        [TestMethod]
+        public void TestComeFromBehind6By4Where1Wins()
+        {
+            Election election = this.GenerateElection(6, 4);
+
+            this.mockRandom.Setup(random => random.Next(2)).Returns(1);
+
+            election.AddBallot(this.FillCandidateNames(1, 2, 3, 4));
+            election.AddBallot(this.FillCandidateNames(2, 1, 3, 4));
+            election.AddBallot(this.FillCandidateNames(3, 1, 4, 2));
+            election.AddBallot(this.FillCandidateNames(3, 4, 1, 2));
+            election.AddBallot(this.FillCandidateNames(4, 1, 3, 2));
+            election.AddBallot(this.FillCandidateNames(4, 2, 3, 1));
+
+            Assert.AreEqual("Candidate 1", election.Winner);
+        }
+
+        /// <summary>
+        /// Tests an election with 3 voters and 3 candidates where all candidates are tied to begin with and
+        /// after candidate 3 is chosen to lose at random, candidate 1 wins.
+        /// 
+        /// Preferences step[0] (Requires random):
+        ///      C1 | C2 | C3
+        ///     --------------
+        ///      1  | 2  | 3
+        ///      2  | 3  | 1
+        ///      3  | 1  | 2
+        ///     --------------
+        /// #1s: 1  | 1  | 1
+        /// Sum: 6  | 6  | 6
+        /// 
+        /// Preferences step[0]:
+        ///      C1 | C2
+        ///     ---------
+        ///      1  | 2
+        ///      1  | 2
+        ///      2  | 1
+        ///     ---------
+        /// #1s: 2  | 1
+        /// Sum: 4  | 5
+        /// </summary>
+        [TestMethod]
+        public void TestTied3By3()
+        {
+            Election election = this.GenerateElection(3, 3);
+
+            this.mockRandom.Setup(random => random.Next(3)).Returns(2);
+
+            election.AddBallot(this.FillCandidateNames(1, 2, 3));
+            election.AddBallot(this.FillCandidateNames(3, 1, 2));
+            election.AddBallot(this.FillCandidateNames(2, 3, 1));
+
+            Assert.AreEqual("Candidate 1", election.Winner);
+        }
+
+        /// <summary>
+        /// Generates a new Election class with the given number of voters and candidates.
+        /// </summary>
+        /// <param name="voters">The desired number of voters in this test.</param>
+        /// <param name="candidates">The desired number of candidates in this test.</param>
+        /// <returns>The election class to test.</returns>
+        private Election GenerateElection(int voters, int candidates)
+        {
+            return new Election(
+                this.GenerateVoters(voters),
+                this.GenerateCandidates(candidates))
+                {
+                    Random = this.mockRandom.Object,
+                };
+        }
+
+        /// <summary>
+        /// Generates a list of voters.
+        /// </summary>
+        /// <param name="count">The number of voters desired.</param>
+        /// <returns>A list of voter names.</returns>
+        private IEnumerable<string> GenerateVoters(int count)
+        {
+            var voters = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                voters.Add("Voter " + (i + 1));
+            }
+
+            return voters;
+        }
+
+        /// <summary>
+        /// Generates a list of candidates.
+        /// </summary>
+        /// <param name="count">The number of candidates desired.</param>
+        /// <returns>A list of candidates names.</returns>
+        private IEnumerable<string> GenerateCandidates(int count)
+        {
+            var candidateNumbers = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                candidateNumbers.Add(i + 1);
+            }
+
+            return this.FillCandidateNames(candidateNumbers.ToArray());
+        }
+
+        /// <summary>
+        /// Generates a list of candidates based off a list of candidate numbers.
+        /// </summary>
+        /// <param name="candidateNumbers">The candidate numbers to use.</param>
+        /// <returns>A list of candidate names.</returns>
+        private IEnumerable<string> FillCandidateNames(params int[] candidateNumbers)
+        {
+            var candidateList = new List<string>();
+            foreach (int candidate in candidateNumbers)
+            {
+                candidateList.Add("Candidate " + candidate);
+            }
+
+            return candidateList;
+        }
+    }
+}
